061f0ba5d8c9a0e5f2c651bfec5c38a1cd864965
Tarrinox <Tarrinox@192.168.1.41>
2018-11-06 21:38:31 +0100

    Con Constructor de Personaje

--- src/PJ/Personaje.java
+++ src/PJ/Personaje.java
@@ -2,5 +2,51 @@ package PJ;
 
 public class Personaje {
 
+	private int hp;
+	private int dmg;
+	private String nom;
+
+	public Personaje(int hp, int dmg, String nom) {
+		super();
+		this.hp = hp;
+		this.dmg = dmg;
+		this.nom = nom;
+	}
+	
+	public Personaje() {
+	
+		this.hp = 0;
+		this.dmg = 0;
+		this.nom = " ";
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getDmg() {
+		return dmg;
+	}
+
+	public void setDmg(int dmg) {
+		this.dmg = dmg;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Personaje [hp=" + hp + ", dmg=" + dmg + ", nom=" + nom + "]";
+	}
 
 }
