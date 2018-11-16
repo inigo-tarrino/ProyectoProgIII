821a5206a5ebb0d0aa9b69139820845fb35a9620
inigo.tarrino <inigo.tarrino@E110L46.opendeusto.es>
2018-11-16 13:49:19 +0100

    mejorado

--- src/Motor/Tesoro.java
+++ src/Motor/Tesoro.java
@@ -6,15 +6,31 @@ import PJ.CofreMi;
 //Seguramente subir al metodo salas especificando que este método se ejecute en las salas
 //de tipo tesoro
 public class Tesoro {
-
+	static int dado= (int) (Math.random()*3)+1;
 	static ArrayList<CofreMi> cofradia = new ArrayList<CofreMi>();
-	CofreMi i;
-	public static void generarcofres(){
-	for (int i = 0; i < 10; i++)
-	{	
+	//CofreMi i;
+	/*
+	static boolean b= false;
+	public static boolean launch(boolean b) 
+	{
+		int ismimic= (int) (Math.random()*2)+1;
+		if(ismimic !=2) 
+		{b= false;
+		}
+		else
+		b= true;
 		
-		CofreMi f1  = new CofreMi(10, false,15,0);
+		return b;
+		
+	}
+	*/
+	public static void generarcofres(){
+	for (int i = 0; i < dado; i++)
+	{	
+	
+		CofreMi f1  = new CofreMi(10,false,15,0);
 		cofradia.add(f1);
+		
 	
 	}
 	}
@@ -23,5 +39,6 @@ public class Tesoro {
 	{
 		generarcofres();
 		System.out.println(cofradia);
+		System.out.println();
 	}
 }
