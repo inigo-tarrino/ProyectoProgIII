11e8aae295e67dcf733eba2bad9eab5b162b7954
Iñigo <Iigo@192.168.1.39>
2018-12-12 23:11:50 +0100

    Clases de Inv implementadas

--- src/Objetos_e_Inventario/Inventario.java
+++ src/Objetos_e_Inventario/Inventario.java
@@ -1,7 +1,28 @@
 package Objetos_e_Inventario;
 
+import java.util.Arrays;
+
 public class Inventario {
 
-	Objeto [] Inventario = new Objeto[10]; 
+	Objeto [] Inventario = new Objeto[10];
+
+	public Inventario(Objeto[] inventario) {
+		super();
+		Inventario = inventario;
+	} 
+	public Inventario() {
+		super();
+		Inventario = null;
+	}
+	public Objeto[] getInventario() {
+		return Inventario;
+	}
+	public void setInventario(Objeto[] inventario) {
+		Inventario = inventario;
+	}
+	@Override
+	public String toString() {
+		return "Inventario [Inventario=" + Arrays.toString(Inventario) + "]";
+	} 
 	
 }
--- src/Objetos_e_Inventario/InventarioTienda.java
+++ src/Objetos_e_Inventario/InventarioTienda.java
@@ -1,5 +1,28 @@
 package Objetos_e_Inventario;
 
+import java.util.Arrays;
+
 public class InventarioTienda {
 
+	Objeto [] InventarioT = new Objeto[10];
+
+	public InventarioTienda(Objeto[] inventarioT) {
+		super();
+		InventarioT = inventarioT;
+	}
+	public InventarioTienda() {
+		super();
+		InventarioT = null;
+	}
+	public Objeto[] getInventarioT() {
+		return InventarioT;
+	}
+	public void setInventarioT(Objeto[] inventarioT) {
+		InventarioT = inventarioT;
+	}
+	@Override
+	public String toString() {
+		return "InventarioTienda [Catálogo=" + Arrays.toString(InventarioT) + "]";
+	}
+	
 }
